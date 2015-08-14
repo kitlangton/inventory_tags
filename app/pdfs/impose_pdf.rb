efeee88e44039004ed3abed3cff9bb553a6f9850
Kit Langton <kitlangton@gmail.com>
2015-08-14 14:01:16 -0400

    Fix impose, cut lines now rendered ABOVE images

--- app/pdfs/impose_pdf.rb
+++ app/pdfs/impose_pdf.rb
@@ -4,20 +4,20 @@ class ImposePdf < Prawn::Document
     @view = view
 
     tags.each_with_index do |tag, i|
-      if (i+5) % 5 == 0
-        stroke do
-          line [132, cursor+10], [132, cursor]
-          line [122, 721], [132, 721]
-
-          line [410, cursor+10], [410, cursor]
-          line [410, 721], [420, 721]
-        end
-      end
+      pre_image_cursor = cursor
 
       image tag.prawn_image,
         width: 278,
         height: 131,
         position: :center
+      stroke do
+        if (i+5) % 5 == 0
+          line [132, pre_image_cursor+10], [132, pre_image_cursor]
+          line [410, pre_image_cursor+10], [410, pre_image_cursor]
+        end
+        line [122, pre_image_cursor], [132, pre_image_cursor]
+        line [410, pre_image_cursor], [420, pre_image_cursor]
+      end
       move_down 10
       stroke do
         line [122, cursor+10], [132, cursor+10]
@@ -25,9 +25,6 @@ class ImposePdf < Prawn::Document
         if ((i+1) % 5 == 0) || (i+1) == tags.size
           line [132, cursor], [132, cursor+10]
           line [410, cursor], [410, cursor+10]
-        else
-          line [122, cursor], [132, cursor]
-          line [410, cursor], [420, cursor]
         end
       end
       if (i+1) % 5 == 0 && (i+1) != tags.size
