867efd7ac47d68fcc26c0cb68089679d2d5d2718
Kit Langton <kitlangton@gmail.com>
2015-08-17 12:42:36 -0400

    Add barcode helper. Update sidekiq Procfile, fixing concurrency.

--- app/helpers/onboarding_helper.rb
+++ app/helpers/onboarding_helper.rb
@@ -1,2 +1,9 @@
+require 'barby'
+require 'barby/barcode/code_128'
+require 'barby/outputter/svg_outputter'
+
 module OnboardingHelper
+  def barby_svg(model)
+    raw(Barby::Code128B.new(model).to_svg(height: 50))
+  end
 end
